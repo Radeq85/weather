214f5df20a6d2c83f6d6329a34ec828afe9720eb
Radek Felicki <r.felicki@gmail.com>
2023-02-16 17:09:01 +0100

    change api key

--- js/weatherFetch.js
+++ js/weatherFetch.js
@@ -4,9 +4,11 @@ export class WeatherFetch {
 
     constructor() {
         this.citySearch = document.querySelector('.searchCity');
+        /// your Api key 
+        this.api = "6a992e023ce1d05f925588a6d549bbb6"
     }
     getWeatherFetch() {
-        return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.citySearch.value}&appid=6a992e023ce1d05f925588a6d549bbb6&units=metric`)
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.citySearch.value}&appid=${this.api}&units=metric`)
             .then((response) => response.json())
             .then((data) => {
                 return data
@@ -14,7 +16,7 @@ export class WeatherFetch {
     }
 
     getWeatherFetchFiveDays() {
-        return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.citySearch.value}&appid=6a992e023ce1d05f925588a6d549bbb6&units=metric`)
+        return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.citySearch.value}&appid=${this.api}&units=metric`)
             .then((response) => response.json())
             .then((data) => {
                 return data
